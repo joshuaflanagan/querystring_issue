42b91ba3deb47e798ae7eb61eb950cefaa16e71c
Joshua Flanagan <joshuaflanagan@gmail.com>
2012-04-24 16:12:14 -0500

    rails new qstring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qstring::Application.config.secret_token = 'c14b24e7d6686fd8c0f7b39809f017c6e62128a7863dfaae44289f3dc8e2ccc3d50bbfac7eef650be84da0da000a31dfb8e2e59ddbfa28e67012c055cebea67a'
