0c5c3d11e859bf32ee094e47d7cc24db96be97d6
Joshua Flanagan <joshuaflanagan@gmail.com>
2012-04-24 22:09:35 -0500

    Demonstrate strange querystring in path generated for decorated model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @decorated_products = Product.all.map{|p| ProductDecorator.new(p)}
   end
+end
+
+class ProductDecorator < DelegateClass(Product)
+
 end
